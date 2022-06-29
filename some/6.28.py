ee0cb59c812b2c820094a71fb69386bc28bee27d
crosspi <mc748159263@outlook.com>
2022-06-29 22:04:43 +0800

    Changes

---
+++ some/6.28.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+pay_1=35.5   #每人缴费
+print(type(pay_1))
+people_1=51#班级学生人数
+men=28
+women=23#男生28人，女生51人
+print(type(people_1))
+print(type(men))
+print(type(women))
+time_1=8
+people_2=30
+bus=2#早上8点出发，每个大巴车坐30人，2辆大巴，
+print(type(time_1))
+print(type(people_2))
+print(type(bus))
+time_2=10+33/60
+time_3=12
+pay_2=25.5
+time_4=15
+pay_3=5 #10点33分进入交大医院，中午12点吃饭食费25.5元，下午3点集体离开，来回大巴费用5元
+print(type(time_2))
+print(type(time_3))
+print(type(pay_2))
+print(type(time_4))
+print(type(pay_3))
+time_5=17
+pay_4=5#晚上5带你返回学校，每人退回5元
+print(type(time_5))
+print(type(pay_4))
